fea8074e6606125bcfceb5e5e3d307d3f9fe2be2
Victor Val <victor.val@avenuecode.com>
2021-12-16 02:10:09 -0300

    script already getting dates of tags , ready to calculate deployment frequency

--- run-metrics.sh
+++ run-metrics.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 while getopts r:o: flag
 do
     case "${flag}" in
@@ -10,10 +12,36 @@ GIT_REPO="https://api.github.com/repos/$OWNER_NAME/$REPO_NAME"
 
 
 
-echo "Repo Name: $REPO_NAME";
-echo "Owner Name: $OWNER_NAME";
+#echo "Repo Name: $REPO_NAME";
+#echo "Owner Name: $OWNER_NAME";
+#echo "git repo: $GIT_REPO";
+
+#exemplo pegando todos os valores de commit.url no repo acima
+#curl -sL $GIT_REPO/tags | jq -r ".[].commit.url"
+
+#curl -sL $GIT_REPO/tags | jq -r ".[].commit.url"
+
+results=$(curl -sL $GIT_REPO/tags | jq -r ".[].commit.sha")
+curl -sL $GIT_REPO/tags | jq -r ".[].name" >> names.txt
+
+ for i in $results; do 
+    echo $GIT_REPO"/commits?sha="$i"&page=1&per_page=1"
+    curl -sL $GIT_REPO"/commits?sha="$i"&page=1&per_page=1" | jq -r ".[].commit.author.date" >> dates.txt;
 
 
-echo "git repo: $GIT_REPO";
+    # echo "Taking 10 seconds for the next request";
 
-curl -sL $GIT_REPO/tags | jq -r ".[].commit.sha"
+    #codigo para esperar dez segundos
+    j=1
+    while [ $j -le 5 ]
+    do
+        sleep 1
+        echo -n ".."$j
+        j=$(($j+1))
+    done
+    echo ""
+    #printado os 10 segundos na tela com espaco
+
+done
+
+echo $dates
