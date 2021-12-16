8fe648d70cb0d2541a7b20e49eab15ca511fef27
Victor Val <victor.val@avenuecode.com>
2021-12-16 03:31:01 -0300

    added test on script if repository exists

--- run-metrics.sh
+++ run-metrics.sh
@@ -10,38 +10,48 @@ done
 
 GIT_REPO="https://api.github.com/repos/$OWNER_NAME/$REPO_NAME"
 
+##VERIFICACAO SE EXISTE REPOSITORIO
+if curl --fail --silent --output /dev/null $GIT_REPO 
+then
+    #NESSE CASO O REPO EXISTE E DEVEMOS RODAR O SCRIPT
 
 
-#echo "Repo Name: $REPO_NAME";
-#echo "Owner Name: $OWNER_NAME";
-#echo "git repo: $GIT_REPO";
+    #echo "Repo Name: $REPO_NAME";
+    #echo "Owner Name: $OWNER_NAME";
+    #echo "git repo: $GIT_REPO";
 
-#exemplo pegando todos os valores de commit.url no repo acima
-#curl -sL $GIT_REPO/tags | jq -r ".[].commit.url"
+    #exemplo pegando todos os valores de commit.url no repo acima
+    #curl -sL $GIT_REPO/tags | jq -r ".[].commit.url"
 
-#curl -sL $GIT_REPO/tags | jq -r ".[].commit.url"
+    #curl -sL $GIT_REPO/tags | jq -r ".[].commit.url"
 
-results=$(curl -sL $GIT_REPO/tags | jq -r ".[].commit.sha")
-curl -sL $GIT_REPO/tags | jq -r ".[].name" >> names.txt
-
- for i in $results; do 
-    echo $GIT_REPO"/commits?sha="$i"&page=1&per_page=1"
-    curl -sL $GIT_REPO"/commits?sha="$i"&page=1&per_page=1" | jq -r ".[].commit.author.date" >> dates.txt;
+    results=$(curl -sL $GIT_REPO/tags | jq -r ".[].commit.sha")
+    curl -sL $GIT_REPO/tags | jq -r ".[].name" >> names.txt
 
 
-    # echo "Taking 10 seconds for the next request";
+    CONT_TAG=1;
 
-    #codigo para esperar dez segundos
-    j=1
-    while [ $j -le 5 ]
-    do
-        sleep 1
-        echo -n ".."$j
-        j=$(($j+1))
+    for i in $results; do 
+        echo "realizando consulta da $CONT_TAG tag...";
+        #echo $GIT_REPO"/commits?sha="$i"&page=1&per_page=1"
+        curl -sL $GIT_REPO"/commits?sha="$i"&page=1&per_page=1" | jq -r ".[].commit.author.date" >> dates.txt;
+        CONT_TAG=$(($CONT_TAG+1))
+
+        #codigo para esperar dez segundos
+        j=1
+        while [ $j -le 5 ]
+        do
+            sleep 1
+            echo -n ".."$j
+            j=$(($j+1))
+        done
+        echo ""
+        #printado os 10 segundos na tela com espaco
+
     done
-    echo ""
-    #printado os 10 segundos na tela com espaco
 
-done
+    echo $dates
 
-echo $dates
+else
+    echo "repo not found!"
+fi
