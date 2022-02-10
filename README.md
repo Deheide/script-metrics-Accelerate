7050306d9121d62628c3e3a5b084162dc0e3938e
Victor Val <victor.val@avenuecode.com>
2022-02-10 19:56:46 -0300

    final version, with all 3 working metrics and README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This script is capable of calculating similar metrics that are found on Accelerate.
 
-## Installation
+## Installation(Pre-requirements)
 
 This project will use [jq](https://stedolan.github.io/jq/download/) to make requests ( with selected parts) for the github API or read the documentation.
 
@@ -11,4 +11,46 @@ To install it simply run:
 ```bash
 sudo apt-get install jq
 ```
+
+
+## Usage
+
+All scripts have a built in flag "-h" that will give a brief description of everything needed to run it.With examples as well. Inside the repo folder you will find two folders , one named: GitUserScripts and other named: ScriptsWithoutToken. The intended usage is with the GitUser and GitToken, as github API calls have an limit for requests per hour wich it is pretty low without authentication. So I encorage the usage of the scripts present on GitUserScripts.
+
+In every script there is present these tags:
+-r  repository name
+-o  owner of a repository name
+
+To make it easily you can understand by taking a look of this repo github url, wich it is:
+https://github.com/Deheide/script-metrics-Accelerate
+
+On it we have the user owner of the repository: Deheide. And followed by the repo name: script-metrics-Accelerate. So the usage of it on a script would be:
+
+
+```bash
+./scriptBeingUsed.sh -o Deheide -r script-metrics-Accelerate
+```
+
+All github repos follow this format: https://github.com/RepoOwner/RepoName
+And the usage would be still:
+
+```bash
+./scriptBeingUsed.sh -o RepoOwner -r RepoName
+```
+
+Every script needs different flags and have different ways to use, but don't worry! As said every script has an -h option that shows all flags present and how to use it. To run this help options just do as it follows:
+
+```bash
+./scriptBeingUsed.sh -h
+```
+
+Now , Its important to know the difference betwen the folders. And here is a simple explanation of the difference between each one: 
+
+### GitUserScripts
+
+These scripts need your github username as well as an github token created within your account. These are used on every request made, and is easily verifiable just taking a look on the code.
+
+### ScriptsWithoutToken
+
+These scripts doesn't use the authentication on the API calls , so sometimes it can stop working as you reach the limit. So its usage is discouraged.
 
---
+++ ScriptsWithoutToken/change-failure-rate.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+##########################PART OF CODE RESPONSIBLE FOR FLAGS SET TO RUN THE CODE#####################
+
+usage()
+{
+  echo -e "-r"'\t'"repository name"
+  echo -e "-o"'\t'"owner of a repository name"
+  echo -e "-l"'\t'"Bug label to search on all issues. E.g: Type:%20Bug ( if the tag was Type: Bug) ; or , Bug (if the tag was just called Bug)"
+  echo -e "-d"'\t'"Starting date to calculate the change failure rate. E.g: 2021-11  .If not set, the default value is the present month"
+  exit 2
+}
+
+STARTING_DATE=$(date +"%Y-%m");
+EXTRA_MONTHS=0;
+
+while getopts d:m:l:u:t:r:o:h flag
+do
+    case "${flag}" in
+        d) STARTING_DATE=${OPTARG};;
+        l) LABEL=${OPTARG};;
+        r) REPO_NAME=${OPTARG};;
+        o) OWNER_NAME=${OPTARG};;
+        h) # Display help.
+            usage
+            exit 0
+        ;;
+    esac
+done
+
+
+#####################################################################################################
+
+GIT_REPO="https://api.github.com/repos/$OWNER_NAME/$REPO_NAME";
+
+if [ -z "$LABEL" ]
+then
+      echo "-l (BUG LABEL) not present. Use -h or read the README for help"
+      exit 0
+fi
+
+##VERYFING IF REPO EXISTS
+if curl --fail --silent --output /dev/null $GIT_REPO
+then
+    #IN THIS CASE THE REPO EXISTS AND WE HAVE TO RUN THE SCRIPT
+    #issuesFoundInPage=$(curl -sL $GIT_REPO"/issues?state=all&since=2010-12-07T09:05:28Z&per_page=100&page=9" | grep created_at);
+    flag=1
+    i=1
+    while [ $flag -eq 1 ]
+    do
+        parcialPageIssues=$(curl -sL $GIT_REPO"/issues?state=all&since=$STARTING_DATE-01T00:00:00Z&per_page=100&page=$i" | grep '"created_at":' | wc -l)
+        if [ $parcialPageIssues -lt 100 ]; then
+            flag=0
+        fi
+        i=$(($i+1));
+        totalPageIssues=$(($totalPageIssues+$parcialPageIssues));
+    done
+
+    flag=1
+    i=1
+    while [ $flag -eq 1 ]
+    do
+        parcialPageIssues=$(curl -sL $GIT_REPO"/issues?state=all;labels=$LABEL&since=$STARTING_DATE-01T00:00:00Z&per_page=100&page=$i" | grep '"created_at":' | wc -l)
+        if [ $parcialPageIssues -lt 100 ]; then
+            flag=0
+        fi
+        i=$(($i+1));
+        totalLabelPageIssues=$(($totalLabelPageIssues+$parcialPageIssues));
+    done
+
+    totalLabelPageIssues=$(($totalLabelPageIssues * 100))
+    #little example on how to get all values json like of the api with format commit.url
+    #curl -sL $GIT_REPO/tags | jq -r ".[].commit.url"
+    result=$(jq -n "$totalLabelPageIssues"/"$totalPageIssues");
+    echo "The calculated change failure rate percentage:" $result"%"
+
+else
+    echo "repo not found or limit of requests exceeded!";
+fi
